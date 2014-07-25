073ec745fed25d6a2e5efa96e367d9040a3ce716
MegaWubs <bwubs@me.com>
2014-07-25 08:57:12 +0200

    Logging of api key

--- examples/js/robin-jsp.js
+++ examples/js/robin-jsp.js
@@ -74,6 +74,7 @@ robin_JSP = {
 		self.elementsList = {};
 		self.querys = {};
 		self.settings.frameUrl = function(){
+			console.log(self.settings.apikey);
 			var url = self.settings.apiurl + '/apikey/' + self.settings.apikey;
 			// if(self.hasRobinConversationID()){
 			// 	url += '/' + self.querys.rbn_cnv;
